931ded12bb606cb05bf77d2fd8b04490852dc0ce
Bogdan Marinescu <bogdan.a.marinescu@intel.com>
2011-07-18 17:36:27 +0300

    added send and receive functions to NRF (not tested yet)

--- src/platform/stm32/nrf_conf.h
+++ src/platform/stm32/nrf_conf.h
@@ -13,10 +13,8 @@
 #define NRF_CFG_TX_POWER      NRF_RF_SETUP_PWR_0
 // Data rate (1Mbps or 2Mbps)
 #define NRF_CFG_DATA_RATE     NRF_RF_SETUP_DR_1MBPS
-// Server pipe 0 address (LSB to MSB) -> only for server 
-#define NRF_CFG_SRV_PIPE0_ADDR  { 0xA1, 0x32, 0x58, 0xF3, 0xC9 }
-// Server pipe 1 address (LSB to MSB) -> only for server
-#define NRF_CFG_SRV_PIPE1_ADDR  { 0xA2, 0x77, 0xC5, 0xEA, 0xB0 }
+// Client address pool (only for clients)
+#define NRF_CLIENT_ADDR_POOL    { 0xC0, 0xBF, 0, 0, 0 }
 
 #endif // #ifndef __NRF_CONF_H__
 
