16233bdd371244dc5e290ba7d82b8e15e036b3b7
Bogdan Marinescu <bogdan.marinescu@gmail.com>
2011-08-07 22:04:18 +0300

    rewritten 'cp' command to handle wildcards properly

--- src/shell.c
+++ src/shell.c
@@ -10,6 +10,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <ctype.h>
 #include "platform.h"
 #include "elua_net.h"
 #include "devman.h"
@@ -55,8 +56,69 @@ static char* shell_prog;
 typedef int ( *p_shell_iterator_cb )( const char *fsname, struct dm_dirent *, void *f, int iterno );
 
 // ****************************************************************************
+// Shell helpers
+
+// Generic FS iterator for 'ls', 'cp' and 'rm'
+static int shellh_pattern_iterator( const char *pattern, p_shell_iterator_cb cb, void *f )
+{
+  const char *pfname;
+  int ptype = cmn_get_path_type( pattern, &pfname );
+  DM_DIR *d;
+  struct dm_dirent *ent;
+  char *pdevname;
+  int i = 0;
+
+  if( ptype == PATH_TYPE_INVALID || ptype == PATH_TYPE_FS )
+    return 0;
+  if( ( pdevname = strndup( pattern, pfname - pattern - 1 ) ) == NULL )
+  {
+    printf( "Not enough memory.\n" );
+    return 0;
+  }
+  if( ( d = dm_opendir( pdevname ) ) != NULL )// iterate and call only matching files
+  {
+    cb( pdevname, NULL, f, -1 );
+    while( ( ent = dm_readdir( d ) ) != NULL )
+      if( cmn_match_fname( ent->fname, pfname ) )
+        if( cb( pdevname, ent, f, i ++ ) == 0 ) // exit iteration indication
+          break;
+    cb( pdevname, NULL, f, i ); // last iteration
+    dm_closedir( d );
+  }
+  printf( TERM_RESET_COL ); // just in case 
+  free( pdevname );
+  return 1;
+}
+
+// Helper: transform a FS name (such as "/rom") into a wildcard pattern ("/rom/*")
+static char* shellh_pattern_from_fs( const char *pfs )
+{
+  char *ppattern;
+
+  if( ( ppattern = malloc( strlen( pfs ) + 3 ) ) == NULL )
+  {
+    printf( "Not enough memory.\n" );
+    return NULL;
+  }
+  strcpy( ppattern, pfs );
+  if( ppattern[ strlen( ppattern ) - 1 ] != '/' )
+    strcat( ppattern, "/" );
+  strcat( ppattern, "*" );
+  return ppattern;
+}
+
+// Print a colorized "[Yes/No/eXit]" prompt
+static void shellh_print_action_prompt()
+{
+  printf( "[%sY%ses/%sN%so/e%sX%sit] ? %s", TERM_FGCOL_LIGHT_YELLOW, TERM_FGCOL_LIGHT_BLUE,
+    TERM_FGCOL_LIGHT_YELLOW, TERM_FGCOL_LIGHT_BLUE,
+    TERM_FGCOL_LIGHT_YELLOW, TERM_FGCOL_LIGHT_BLUE, TERM_RESET_COL );
+}
+
+// ****************************************************************************
 // Shell functions
 
+// ----------------------------------------------------------------------------
 // 'help' handler
 static void shell_help( char* args )
 {
@@ -69,13 +131,17 @@ static void shell_help( char* args )
   printf( "  lua [args]  - run Lua with the given arguments\n" );
   printf( "  recv [path] - receive a file via XMODEM, if there is a path, save"
           "                there, otherwise run it.");
-  printf( "  cp <src> <dst> - copy source file 'src' to 'dst'\n" );
+  printf( "  cp <src> <dst> [-b] [-c] [-o] - copy source file 'src' to 'dst'\n" );
+  printf( "     -b: backup mode (prepend the destination file name with '_b').\n" );
+  printf( "     -c: ask for confirmation before copying a file.\n" );
+  printf( "     -o: overwrite destination files without confirmation.\n" );
   printf( "  ee <file>   - dump file to the EEPROM connected on I2C1\n" );
   printf( "  edit <file> - edits the given file\n" );
   printf( "  ver         - print eLua version\n" );
-  printf( "  rm <file> <-f> - removes the file, use '-f' to supress confirmation\n" );
+  printf( "  rm <file> [-f] - removes the file, use '-f' to supress confirmation\n" );
 }
 
+// ----------------------------------------------------------------------------
 // 'lua' handler
 static void shell_lua( char* args )
 {
@@ -122,6 +188,7 @@ static void shell_lua( char* args )
   clearerr( stdin );
 }
 
+// ----------------------------------------------------------------------------
 // 'recv' handler
 static void shell_recv( char* args )
 {
@@ -199,6 +266,7 @@ static void shell_recv( char* args )
 #endif // #ifndef BUILD_XMODEM
 }
 
+// ----------------------------------------------------------------------------
 // 'ver' handler
 static void shell_ver( char* args )
 {
@@ -207,38 +275,7 @@ static void shell_ver( char* args )
   printf( "For more information visit www.eluaproject.net and wiki.eluaproject.net\n" );
 }
 
-// Generic FS iterator for 'ls', 'cp' and 'rm'
-static int shellh_pattern_iterator( const char *pattern, p_shell_iterator_cb cb, void *f )
-{
-  const char *pfname;
-  int ptype = cmn_get_path_type( pattern, &pfname );
-  DM_DIR *d;
-  struct dm_dirent *ent;
-  char *pdevname;
-  int i = 0;
-
-  if( ptype == PATH_TYPE_INVALID || ptype == PATH_TYPE_FS )
-    return 0;
-  if( ( pdevname = strndup( pattern, pfname - pattern - 1 ) ) == NULL )
-  {
-    printf( "Not enough memory.\n" );
-    return 0;
-  }
-  if( ( d = dm_opendir( pdevname ) ) != NULL )// iterate and call only matching files
-  {
-    cb( pdevname, NULL, f, -1 );
-    while( ( ent = dm_readdir( d ) ) != NULL )
-      if( cmn_match_fname( ent->fname, pfname ) )
-        if( cb( pdevname, ent, f, i ++ ) == 0 ) // exit iteration indication
-          break;
-    cb( pdevname, NULL, f, i ); // last iteration
-    dm_closedir( d );
-  }
-  printf( TERM_RESET_COL ); // just in case 
-  free( pdevname );
-  return 1;
-}
-
+// ----------------------------------------------------------------------------
 // 'ls' and 'dir' handler
 // Iterator callback
 static int shell_ls_iterator_cb( const char *fsname, struct dm_dirent *ent, void *f, int i )
@@ -313,6 +350,7 @@ static void shell_ls( char* args )
   printf( "\n" );
 }
 
+// ----------------------------------------------------------------------------
 // 'cat' and 'type' handler
 static void shell_cat( char *args )
 {
@@ -347,8 +385,9 @@ static void shell_cat( char *args )
   }
   else
     printf( "Usage: cat (or type) <filename1> [<filename2> ...]\n" );
-}    
+}
 
+// ----------------------------------------------------------------------------
 // 'edit' handler
 static void shell_edit( char *args )
 {
@@ -358,6 +397,7 @@ static void shell_edit( char *args )
   editor_mainloop();
 }
 
+// ----------------------------------------------------------------------------
 // 'rm' handler
 // Iterator callback
 static int shell_rm_iterator_cb( const char *fsname, struct dm_dirent *ent, void *f, int i )
@@ -375,8 +415,8 @@ static int shell_rm_iterator_cb( const char *fsname, struct dm_dirent *ent, void
   snprintf( fullpattern, DM_MAX_DEV_NAME + DM_MAX_FNAME_LENGTH + 1, "%s/%s", fsname, ent->fname );
   if( *pask )
   {
-    printf( TERM_FGCOL_LIGHT_BLUE "Delete %s [%sY%ses/%sN%so/e%sX%sit] ? " TERM_RESET_COL, fullpattern, TERM_FGCOL_LIGHT_YELLOW,
-      TERM_FGCOL_LIGHT_BLUE, TERM_FGCOL_LIGHT_YELLOW, TERM_FGCOL_LIGHT_BLUE, TERM_FGCOL_LIGHT_YELLOW, TERM_FGCOL_LIGHT_BLUE );
+    printf( TERM_FGCOL_LIGHT_BLUE "Delete %s ", fullpattern );
+    shellh_print_action_prompt();
     resp = term_getch( TERM_INPUT_WAIT );
     printf( "%c\n", ( char )resp );
     if( resp == 'x' || resp == 'X' )
@@ -424,17 +464,10 @@ static void shell_rm( char *args )
       ptype = cmn_get_path_type( args, NULL );
       if( ptype == PATH_TYPE_INVALID )
         printf( "Invalid argument '%s'.\n", args );
-      else if( ptype == PATH_TYPE_FS ) // forcs <fs>/* pattern
+      else if( ptype == PATH_TYPE_FS ) // force <fs>/* pattern
       {
-        if( ( pdevname = malloc( strlen( args ) + 3 ) ) == NULL )
-        {
-          printf( "Not enough memory.\n" );
+        if( ( pdevname = shellh_pattern_from_fs( args ) ) == NULL )
           return;
-        }
-        strcpy( pdevname, args );
-        if( pdevname[ strlen( pdevname ) - 1 ] != '/' )
-          strcat( pdevname, "/" );
-        strcat( pdevname, "*" );
         pattern = pdevname;
       }
       else
@@ -448,6 +481,7 @@ static void shell_rm( char *args )
     free( pdevname );
 }
 
+// ----------------------------------------------------------------------------
 // 'ee' handler
 static void shell_ee( char *args )
 {
@@ -502,76 +536,247 @@ static void shell_ee( char *args )
   fclose( fp );
 }
 
+// ----------------------------------------------------------------------------
 // 'copy' handler
 #ifdef BUILD_RFS
 #define SHELL_COPY_BUFSIZE    ( ( 1 << RFS_BUFFER_SIZE ) - ELUARPC_WRITE_REQUEST_EXTRA )
 #else
 #define SHELL_COPY_BUFSIZE    256
 #endif
-static void shell_cp( char *args )
+#define SHELL_CP_FLAG_BACKUP    1
+#define SHELL_CP_FLAG_CONFIRM   2
+#define SHELL_CP_FLAG_OVERWRITE 4
+
+// Copy state (for callback)
+typedef struct
 {
-  char *p1, *p2;
+  int srctype, desttype;
+  char *psrc, *pdest;
+  const char *psrcname;
+  int flags;
+} shell_cp_state;
+
+// Copy helper: makes the actual copy
+static int shellh_cp_copy( const char *psrcname, const char *pdestname, int flags )
+{
   int res = 0;
   FILE *fps = NULL, *fpd = NULL;
   void *buf = NULL;
-  size_t datalen, total = 0;
+  size_t datalen;
 
-  if( *args )
+  // Check if the file exists first
+  if( ( fps = fopen( psrcname, "rb" ) ) == NULL )
   {
-    p1 = strchr( args, ' ' );
-    if( p1 )
+    printf( TERM_FGCOL_LIGHT_RED "Unable to open %s for reading\n" TERM_RESET_COL, psrcname );
+    goto cphout;
+  }
+  // If it exists and confirmation mode is on, ask for confirmation
+  if( flags & SHELL_CP_FLAG_CONFIRM )
+  {
+    printf( TERM_FGCOL_LIGHT_BLUE "Copy file %s to %s ", psrcname, pdestname );
+    shellh_print_action_prompt();
+    printf( "%c\n", res = term_getch( TERM_INPUT_WAIT ) );
+    if( toupper( res ) == 'X' ) // exit copy operation
     {
-      *p1 = 0;
-      p2 = strchr( p1 + 1, ' ' );
-      if( p2 )
+      res = -1;
+      goto cphout;
+    }
+    else if( toupper( res ) != 'Y' ) // skip this file
+    {
+      res = 1;
+      goto cphout;
+    }
+    else
+      res = 0;
+  }
+  // If the destination exists and 'overwrite' is off, ask for confirmation
+  if( ( ( flags & SHELL_CP_FLAG_OVERWRITE ) == 0 ) && ( ( fpd = fopen( pdestname, "rb" ) ) != NULL ) )
+  {
+    fclose( fpd );
+    fpd = NULL;
+    printf( TERM_FGCOL_LIGHT_BLUE "File '%s' exists, overwrite ", pdestname );
+    shellh_print_action_prompt();
+    printf( "%c\n", res = term_getch( TERM_INPUT_WAIT ) );
+    if( toupper( res ) == 'X' ) // exit copy operation
+    {
+      res = -1;
+      goto cphout;
+    }
+    else if( toupper( res ) != 'Y' ) // skip this file
+    {
+      res = 1;
+      goto cphout;
+    }
+    else
+      res = 0;
+  }  
+  if( ( fpd = fopen( pdestname, "wb" ) ) == NULL )
+    printf( TERM_FGCOL_LIGHT_RED "Unable to open %s for writing\n" TERM_RESET_COL, pdestname );
+  else
+  {
+    // Alloc memory
+    if( ( buf = malloc( SHELL_COPY_BUFSIZE ) ) == NULL )
+      printf( TERM_FGCOL_LIGHT_RED "Not enough memory\n" TERM_RESET_COL );
+    else
+    {
+      // Do the actual copy
+      printf( TERM_FGCOL_LIGHT_GREEN "Copying %s to %s ... ", psrcname, pdestname );
+      while( 1 )
       {
-        *p2 = 0;
-        // First file is at args, second one at p1 + 1
-        if( ( fps = fopen( args, "rb" ) ) == NULL )
-          printf( "Unable to open %s for reading\n", args );
-        else
+        datalen = fread( buf, 1, SHELL_COPY_BUFSIZE, fps );
+        if( fwrite( buf, 1, datalen, fpd ) != datalen )
         {
-          if( ( fpd = fopen( p1 + 1, "wb" ) ) == NULL )
-            printf( "Unable to open %s for writing\n", p1 + 1 );
-          else
-          {
-            // Alloc memory
-            if( ( buf = malloc( SHELL_COPY_BUFSIZE ) ) == NULL )
-              printf( "Not enough memory\n" );
-            else
-            {
-              // Do the actual copy
-              while( 1 )
-              {
-                datalen = fread( buf, 1, SHELL_COPY_BUFSIZE, fps );
-                if( fwrite( buf, 1, datalen, fpd ) != datalen )
-                {
-                  printf( "Unable to write to %s\n", p1 + 1 );
-                  goto out;
-                }
-                total += datalen;
-                if( datalen < SHELL_COPY_BUFSIZE )
-                  break;
-              }
-              printf( "%u bytes copied\n", ( unsigned int )total );
-              res = 1;
-            }
-          }
-        } 
+          printf( TERM_FGCOL_LIGHT_RED "unable to write to %s\n" TERM_RESET_COL, pdestname );
+          goto cphout;
+        }
+        if( datalen < SHELL_COPY_BUFSIZE )
+          break;
       }
+      printf( TERM_FGCOL_LIGHT_BLUE "done\n" TERM_RESET_COL );
+      res = 1;
     }
   }
-out:  
-  if( !res )
-    printf( "Copy error.\n" );
+cphout:
   if( fps )
     fclose( fps );
   if( fpd )
     fclose( fpd );
   if( buf )
     free( buf );
+  return res;
 }
 
+// Copy helper: make a full name from a FS name and a file name. If 'backup' is not 0 
+// prepend the file name with a "b_"
+static void shellh_cp_makefname( char *pdest, const char *pfsname, const char *fname, int backup )
+{
+  strcpy( pdest, pfsname );
+  if( pdest[ strlen( pdest ) -1 ] == '/' )
+    pdest[ strlen( pdest ) - 1 ] = '\0';
+  strcat( pdest, "/" );
+  strcat( pdest, backup ? "b_" : "" );
+  strcat( pdest, fname );
+}
+
+// Iterator callback
+static int shell_cp_iterator_cb( const char *fsname, struct dm_dirent *ent, void *f, int i )
+{
+  shell_cp_state *pcs = ( shell_cp_state* )f;
+  char fulldest[ DM_MAX_DEV_NAME + DM_MAX_FNAME_LENGTH + 4 ];
+  char fullsrc[ DM_MAX_DEV_NAME + DM_MAX_FNAME_LENGTH + 2 ];
+  int res;
+
+  if( ent == NULL )
+    return 1;
+  if( pcs->srctype == PATH_TYPE_FILE )
+  {
+    if( pcs->desttype == PATH_TYPE_FILE ) // simple copy, ignore backup flag
+      res = shellh_cp_copy( pcs->psrc, pcs->pdest, pcs->flags );
+    else // make dest name same as source name
+    {
+      shellh_cp_makefname( fulldest, pcs->pdest, pcs->psrcname, pcs->flags & SHELL_CP_FLAG_BACKUP );
+      res = shellh_cp_copy( pcs->psrc, fulldest, pcs->flags );
+    }
+  }
+  else // wildcard
+  {
+    snprintf( fullsrc, DM_MAX_FNAME_LENGTH + 1, "%s/%s", fsname, ent->fname );
+    shellh_cp_makefname( fulldest, pcs->pdest, ent->fname, pcs->flags & SHELL_CP_FLAG_BACKUP );
+    res = shellh_cp_copy( fullsrc, fulldest, pcs->flags );
+  }
+  return res == -1 ? 0 : 1;
+}
+
+static void shell_cp( char *args )
+{
+  int argno = 0;
+  char *psrc = NULL, *pdest = NULL, *p;
+  int flags = 0;
+  int srctype, desttype;
+  const char *srcname = NULL;
+  char *srcwc = NULL;
+  shell_cp_state cs;
+
+  // Collect all arguments
+  if ( *args )
+  {
+    while ( *args ) 
+    {
+      p = strchr( args, ' ' );
+      *p = 0;
+      if( argno == 0 )
+        psrc = args;
+      else if( argno == 1 )
+        pdest = args;
+      else
+      {
+        if( !strcasecmp( args, "-b" ) )
+          flags |= SHELL_CP_FLAG_BACKUP;
+        else if( !strcasecmp( args, "-c" ) )
+          flags |= SHELL_CP_FLAG_CONFIRM;
+        else if( !strcasecmp( args, "-o" ) )
+          flags |= SHELL_CP_FLAG_OVERWRITE;
+        else
+        {
+          printf( TERM_FGCOL_LIGHT_RED "Invalid argument '%s'\n" TERM_RESET_COL, args );
+          return;
+        }
+      }
+      args = p + 1;
+      argno ++;
+    }      
+  }
+  if( !psrc || !pdest )
+  {
+    printf( TERM_FGCOL_LIGHT_RED "Must specify both source and destination\n" TERM_RESET_COL );
+    return;
+  }
+  srctype = cmn_get_path_type( psrc, &srcname );
+  desttype = cmn_get_path_type( pdest, NULL );
+  // Check source
+  if( srctype == PATH_TYPE_INVALID )
+  {
+    printf( TERM_FGCOL_LIGHT_RED "Invalid source.\n" TERM_RESET_COL );
+    return;
+  }
+  else if( srctype == PATH_TYPE_FS ) // transform this into a pattern
+  {
+    if( ( psrc = srcwc = shellh_pattern_from_fs( psrc ) ) == NULL )
+      return;
+    srctype = cmn_get_path_type( psrc, &srcname );
+  }
+  // Check destination
+  if( desttype == PATH_TYPE_INVALID || desttype == PATH_TYPE_WC )
+  {
+    printf( TERM_FGCOL_LIGHT_RED "Invalid destination.\n" TERM_RESET_COL );
+    goto cpdone;
+  }
+  if( srctype == PATH_TYPE_WC && desttype == PATH_TYPE_FILE )
+  {
+    printf( TERM_FGCOL_LIGHT_RED "Invalid source/destination combination.\n" TERM_RESET_COL );
+    goto cpdone;
+  }
+  // Check for invalid backup flag
+  if( desttype == PATH_TYPE_FILE && ( ( flags & SHELL_CP_FLAG_BACKUP ) != 0 ) )
+  {
+    printf( TERM_FGCOL_LIGHT_RED "Cannot use the backup flag with a specific destination.\n" TERM_RESET_COL );
+    goto cpdone;
+  }
+  // Prepares state for iteration
+  cs.srctype = srctype;
+  cs.desttype = desttype;
+  cs.psrc = psrc;
+  cs.pdest = pdest;
+  cs.psrcname = srcname;
+  cs.flags = flags;
+  shellh_pattern_iterator( psrc, shell_cp_iterator_cb, &cs );
+cpdone:
+  if( srcwc )
+    free( srcwc );
+}
+
+// ----------------------------------------------------------------------------
+// Shell command table
 // Insert shell commands here
 static const SHELL_COMMAND shell_commands[] =
 {
